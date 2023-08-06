0ae2bf1a4911915c26c75bb5c2beb9542f16df59
Dimka <125756956+f-dimas-pl@users.noreply.github.com>
2023-08-07 00:33:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ $(function(){
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="#"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="#"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="#"></button>',
+        responsive: [
+            {
+              breakpoint: 968,
+              settings: {
+                arrows: false
+              }
+            },
+          ]
     });
     
     $('.menu__btn').on('click', function(){
