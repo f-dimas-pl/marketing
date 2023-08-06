359d1813519011da7d33dc0d3298799a44eb9951
Dimka <f.dimas.pl@gmail.com>
2023-08-06 15:44:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+    
+    $('.slider-blog__inner').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="#"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="#"></button>'
+    });
+    
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+    
+
+
+
+
+
+    var mixer = mixitup('.portfolio__content');
+});
